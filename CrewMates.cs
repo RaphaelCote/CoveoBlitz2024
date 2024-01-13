be9f26cb8bf04c8c960758a0c3e28862382fdaac
Ralmog <raphaelcote20@gmail.com>
2024-01-13 10:40:27 -0500

    Update CrewMates.cs

--- CrewMates.cs
+++ CrewMates.cs
@@ -12,7 +12,7 @@ namespace Application
         public CrewMates() { }
 
         /// <summary>
-        /// Move a idle crew to station
+        /// Move a closest idle crew to a station
         /// </summary>
         /// <param name="gameMessage"></param>
         /// <param name="station"></param>
@@ -72,4 +72,67 @@ namespace Application
 
             return false;
         }
+
+        /// <summary>
+        /// Move a closest idle crew to a closest station of type
+        /// </summary>
+        /// <param name="gameMessage"></param>
+        /// <param name="station"></param>
+        /// <param name="moveAction"></param>
+        /// <returns>
+        /// True : Action to move crew created
+        /// False : Now idle crew, try liberating a crew from less important task
+        /// </returns>
+        static public bool MoveClosestCrewToStationType(GameMessage gameMessage, Station station, out Action? moveAction)
+        {
+            var myShip = gameMessage.Ships[gameMessage.CurrentTeamId];
+
+            //All idle crewmates
+            List<Crewmate> idleCrewmates = myShip.Crew
+            .Where(crewmate => crewmate.CurrentStation == null && crewmate.Destination == null)
+            .ToList();
+
+            //Look for closer crew
+            if(idleCrewmates.Count > 0)
+            {
+
+                //Station stationToGo = myShip.Stations.
+                Crewmate closestCrew = idleCrewmates[0];
+                int crewmateDistance = -1;
+                foreach(var crewmate in idleCrewmates)
+                {
+                    List<DistanceFromStation> visitableStations = crewmate.DistanceFromStations.Shields
+                        .Concat(crewmate.DistanceFromStations.Turrets)
+                        .Concat(crewmate.DistanceFromStations.Helms)
+                        .Concat(crewmate.DistanceFromStations.Radars)
+                        .ToList();
+
+                    int distance = visitableStations
+                        .Where(distanceFromStation => distanceFromStation.StationId == station.Id)
+                        .FirstOrDefault()
+                        .Distance;
+
+                    if(crewmateDistance == -1)
+                    {
+                        closestCrew = crewmate;
+                        crewmateDistance = distance;
+                    }
+                    else if(distance < crewmateDistance)
+                    {
+                        closestCrew = crewmate;
+                        crewmateDistance = distance;
+                    }
+
+                }
+
+                moveAction = new CrewMoveAction(closestCrew.Id, station.GridPosition);
+
+                return true;
+            }
+
+            moveAction = null;
+
+            return false;
+        }
     }
+}

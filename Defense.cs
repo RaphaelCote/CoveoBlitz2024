3a7a32cf67e1e25dbcdd26715836ebc35462c604
Jabooby <turj3113@usherbrooke.ca>
2024-01-13 11:23:32 -0500

    Defense update

--- Defense.cs
+++ Defense.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application
+namespace Application;
+public class Defense
 {
-    internal class Defense
+    public static class Shield
     {
+        public static int Priority { get; set;}
+        public static int ValueMax {get; set;}
+        public static int CalculatePriority(GameMessage gameMessage)
+        {
+            var myShip = gameMessage.Ships[gameMessage.CurrentTeamId];
+            ValueMax = gameMessage.Constants.Ship.MaxShield;
+            double percentShield = (myShip.CurrentShield/ValueMax)*100;
+            if(percentShield < 20)
+                Priority = 5;
+            else if(percentShield < 60)
+                Priority = 3;
+            else
+                Priority = 0;
+            return Priority;
+        }
+    }
+    
+
+    public Defense()
+    {
+
     }
 }
